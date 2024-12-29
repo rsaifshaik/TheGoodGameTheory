5ebaf6e41c0be94e63e7a507df6311343cdd6957
Shaik Saif <rsaifshaik234@gmail.com>
2024-12-29 12:48:42 +0530

    icon added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'dart:async';
 
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,30 +18,50 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late StreamSubscription _intentSub;
   final _sharedFiles = <SharedMediaFile>[];
+  String url = 'https://www.gameyoutube.com/watch?v=u0fHCUj7kP4';
+
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(
+      Uri.parse('https://www.gameyoutube.com/watch?v=u0fHCUj7kP4'),
+    );
 
   @override
   void initState() {
     super.initState();
-
     _intentSub = ReceiveSharingIntent.instance.getMediaStream().listen((value) {
-      setState(() {
-        _sharedFiles.clear();
-        _sharedFiles.addAll(value);
-
-        print(_sharedFiles.map((f) => f.toMap()));
-      });
+      setState(
+        () {
+          _sharedFiles.clear();
+          _sharedFiles.addAll(value);
+          log(
+            _sharedFiles.map((f) => f.toMap()).toString(),
+          );
+        },
+      );
     }, onError: (err) {
       print("getIntentDataStream error: $err");
     });
 
-    ReceiveSharingIntent.instance.getInitialMedia().then((value) {
-      setState(() {
-        _sharedFiles.clear();
-        _sharedFiles.addAll(value);
-        print(_sharedFiles.map((f) => f.toMap()));
-        ReceiveSharingIntent.instance.reset();
-      });
-    });
+    ReceiveSharingIntent.instance.getInitialMedia().then(
+      (value) {
+        log(
+          value.length.toString(),
+        );
+        setState(
+          () {
+            _sharedFiles.clear();
+            _sharedFiles.addAll(value);
+            if (_sharedFiles.isNotEmpty)
+              controller.loadRequest(
+                Uri.parse('https://www.gameyoutube.com/watch?v=' +
+                    "${_sharedFiles.first.path.replaceAll('https://youtube.com/watch?v=', '')}"),
+              );
+            ReceiveSharingIntent.instance.reset();
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -51,34 +74,35 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     const textStyleBold = const TextStyle(fontWeight: FontWeight.bold);
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
           backgroundColor: Colors.grey,
-            appBar: AppBar(
-              backgroundColor: Colors.grey,
-              title: const Text('Quiz App'),
-            ),
-            body: Center(
-              child: Column(
-                children: <Widget>[
-                  const Text("Shared files:", style: textStyleBold),
-                  Expanded(
-                    child: ListView.builder(
-                        itemCount: _sharedFiles.length,
-                        itemBuilder: (context,index){
-                          SharedMediaFile sharedFile=_sharedFiles[index];
-                          if(sharedFile.type==SharedMediaType.image){
-                            return Image.file(File(sharedFile.path));
-                          }else{
-                            return Text(sharedFile.path);
-                          }
-                        }
-                    ),
-                  )
-                ],
-              ),
-            ),
+          title: const Text('Quiz App'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Shared files: ${_sharedFiles.length}",
+                  style: textStyleBold),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _sharedFiles.length,
+                  itemBuilder: (context, index) {
+                    SharedMediaFile sharedFile = _sharedFiles[index];
+                    return SizedBox(
+                      height: 800,
+                      width: 300,
+                      child: WebViewWidget(controller: controller),
+                    );
+                  },
+                ),
+              )
+            ],
           ),
-        );
-    }
+        ),
+      ),
+    );
+  }
 }
